fd3d87e77376c22489dfdc65ea2047081a76d6bf
Evgenia Kositsyna <kositzina.eugenia@yandex.ru>
2023-08-02 17:40:01 +0300

    fix_mistakes

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@ arr_=[]
 for i in range(N):
     arr_.append(int(input()))
 def pars(n, arr_raw):
-    ans=[]
+ 
     arr=sorted(arr_raw)
     k=0
     min=0
@@ -11,14 +11,10 @@ def pars(n, arr_raw):
     for i in range(len(arr)):
         if min!=None: 
             min=arr[i]-1
-        sr=arr[i]
-        max=arr[i]+1
         new_arr[0]=arr[i-1]-1
         new_arr[1]=arr[i-1]
         new_arr[2]=arr[i-1]+1
         min_count=new_arr.count(min)
-        
-        
         if min_count!=0 :
             min=None
             continue
